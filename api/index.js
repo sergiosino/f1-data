51db8dadc74a2777af6ccbd572d1496ac5ef350d
Sergio Iglesias Traviesa <sergiosino96@hotmail.com>
2023-05-21 12:57:05 +0200

    First api test endpoint

--- api/index.js
+++ api/index.js
@@ -1,8 +1,22 @@
 import { Hono } from 'hono'
 import gps from 'database/gps.json'
+// import gpsInfo from 'database/gpsInfo.json'
+// import gpsResults from 'database/gpsResults.json'
 
 const app = new Hono()
 
-app.get('/', ctx => ctx.json(gps))
+app.get('/', (ctx) =>
+    ctx.json([
+        {
+            name: 'gps',
+            endpoint: '/gps',
+            description: 'Returns all the GPs of the season'
+        }
+    ])
+)
+
+app.get('/gps', (ctx) => {
+    return ctx.json(gps)
+})
 
 export default app
