64a7e338677048b1cb07be8db77bc54f811db4cc
Sergio Iglesias Traviesa <sergiosino96@hotmail.com>
2023-05-26 21:25:06 +0200

    Added cors to api

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import gps from 'database/gps.json'
 // import gpsInfo from 'database/gpsInfo.json'
 // import gpsResults from 'database/gpsResults.json'
 
 const app = new Hono()
+
+app.use(cors({ origin: '*' }))
 
 app.get('/', (ctx) =>
     ctx.json([
