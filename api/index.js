33548c0be204c046b0f1d487988a9a4c72fbabc8
Sergio Iglesias Traviesa <sergiosino96@hotmail.com>
2023-05-28 16:08:47 +0200

    List gps endpoint

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import gps from 'database/gps.json'
-// import gpsInfo from 'database/gpsInfo.json'
-// import gpsResults from 'database/gpsResults.json'
+import gpsInfo from 'database/gpsInfo.json'
+import gpsResults from 'database/gpsResults.json'
 
 const app = new Hono()
 
@@ -19,7 +19,48 @@ app.get('/', (ctx) =>
 )
 
 app.get('/gps', (ctx) => {
-    return ctx.json(gps)
+    const response = []
+
+    gps.forEach(gp => {
+        const { id, round, image, name, circuitName } = gp
+
+        // Prepare gp info
+        const gpInfo = gpsInfo.find(x => x.id === id)
+        const start = gpInfo.calendar[0].start
+        const end = gpInfo.calendar[gpInfo.calendar.length - 1].end
+
+        // Prepare gp results
+        const gpResults = gpsResults.find(x => x.id === id)
+        const isCompleted = gpResults.isCompleted
+        let topThree = []
+        if(isCompleted) {
+            const gpRaceResults = gpResults.events.find(x => x.shortName === 'Race')?.results ?? []
+            const gpRaceResultsTopThree = gpRaceResults.slice(0, 3)
+            topThree = gpRaceResultsTopThree.map(({rank, driver}) => {
+                const { nameCode } = driver
+                const nameCodeLowerCase = nameCode.toLowerCase()
+                return {
+                    rank,
+                    nameCode,
+                    image: `${nameCodeLowerCase}.avif`
+                }
+            })
+        }
+
+        response.push({
+            id,
+            round,
+            image,
+            name,
+            circuitName,
+            start,
+            end,
+            isCompleted,
+            topThree
+        })
+    })
+    
+    return ctx.json(response)
 })
 
 export default app
