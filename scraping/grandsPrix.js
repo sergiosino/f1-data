ed2253d4d435c17d473a7583a82dddcb744bd325
Sergio Iglesias Traviesa <sergiosino96@hotmail.com>
2023-05-28 13:28:03 +0200

    Gp day and month removed from gps db

--- scraping/grandsPrix.js
+++ scraping/grandsPrix.js
@@ -16,17 +16,12 @@ export async function getGrandsPrix() {
         const gpImage = $tableRow.find('.table__text--primary > img').prop('src').split('/').reverse()[0]
         const gpName = $tableRow.find('.table__text--primary > strong').text()
         const circuitName = $tableRow.find('td > .table__text--secondary').text()
-        const date = $tableRow.find('.table__text--date > .table__text--primary').text().split(' ')
-        const day = date[0]
-        const month = date[1]
 
         gps.push({
             id: gpId,
             gpImage,
             gpName,
             circuitName,
-            day,
-            month
         })
     }
 
